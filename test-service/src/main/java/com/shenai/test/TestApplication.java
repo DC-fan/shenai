ac8853b482428f0ccfad60f464f9bfec37fe37b9
dachui <1042804103@qq.com>
2020-09-17 23:40:03 +0800

    first commit

---
+++ test-service/src/main/java/com/shenai/test/TestApplication.java
@@ -0,0 +1,14 @@
+package com.shenai.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
